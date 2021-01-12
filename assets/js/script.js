004b3ef31723400995da9a9949d37d106d4daf3d
Kip HudaKoz <kip.hudakoz@me.com>
2021-01-11 20:39:14 -0500

    addEventListenerfor even

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,13 @@
-//TARGETING HTML ELEMENT AND ADDING TO JAVASCRIPT
-var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
+var formEl = document.querySelector("#task-form"); 
+var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
+var createTaskHandler = function(event) { 
+  event.preventDefault(); 
 
-  var buttonEl = document.querySelector("#save-task");
-  var tasksToDoEl = document.querySelector("#tasks-to-do");
-
-  var createTaskHandler = function() { 
   var listItemEl = document.createElement("li"); 
   listItemEl.className = "task-item"; 
   listItemEl.textContent = "This is a new task."; 
   tasksToDoEl.appendChild(listItemEl); 
   }; 
 
-buttonEl.addEventListener("click", createTaskHandler);
-
-
-
+  formEl.addEventListener("submit", createTaskHandler);
